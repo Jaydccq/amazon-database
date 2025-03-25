323bcb82aacdd2009cf66350ead2d4e73609807a
Jiaxin-Cindy <jiaxin.gao1997@gmail.com>
2025-03-25 09:08:58 -0400

    modify

--- app/config.py
+++ app/config.py
@@ -3,12 +3,17 @@ from urllib.parse import quote_plus
 
 
 class Config(object):
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
     SECRET_KEY = 'gingerbread-miniamazon-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'\
-        .format(os.environ.get('DB_USER'),
-                quote_plus(os.environ.get('DB_PASSWORD')),
-                os.environ.get('DB_HOST'),
-                os.environ.get('DB_PORT'),
-                os.environ.get('DB_NAME'))
+
+    password = os.environ.get('DB_PASSWORD', '')
+    password_encoded = quote_plus(password.encode('utf-8'))
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(
+        os.environ.get('DB_USER', ''),
+        password_encoded,
+        os.environ.get('DB_HOST', 'localhost'),
+        os.environ.get('DB_PORT', '5432'),
+        os.environ.get('DB_NAME', '')
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
